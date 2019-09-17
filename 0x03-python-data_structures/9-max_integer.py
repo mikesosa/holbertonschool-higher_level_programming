0dabf857b959ee6f1cef35e591f8dd87d01275e1
Michael <michael.sosa@holbertonschool.com>
2019-09-17 18:05:28 -0500

    I think is not allowed to reorganize the list

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -7,10 +7,7 @@ def max_integer(my_list=[]):
         j = 0
         while j < i:
             if my_list[j] > my_list[j + 1]:
-                tmp = my_list[j]
-                my_list[j] = my_list[j + 1]
-                my_list[j + 1] = tmp
+                result = my_list[j]
             j += 1
         i -= 1
-    result = my_list[-1:]
     return result
