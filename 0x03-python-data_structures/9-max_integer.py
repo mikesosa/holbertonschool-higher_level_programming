94d9d6947bcbde82bbf02187a11fb4eae22484c4
Michael <michael.sosa@holbertonschool.com>
2019-09-17 17:59:37 -0500

    Apparently sort is not allowed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    my_list.sort()
-    return(my_list[-1:])
+    if my_list is None:
+        return None
+    i = len(my_list) - 1
+    while i > 1:
+        j = 0
+        while j < i:
+            if my_list[j] > my_list[j + 1]:
+                tmp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = tmp
+            j += 1
+        i -= 1
+    return my_list[-1:]
