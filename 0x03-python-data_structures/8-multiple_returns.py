178e7984e1d8150eea6903382865db7298918cd2
Michael <michael.sosa@holbertonschool.com>
2019-09-17 17:41:26 -0500

    Update is sentence empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,6 @@
 def multiple_returns(sentence):
     if sentence is None:
         return None
-    strlen = len(sentence)
-    strfchar = sentence[0]
-    if sentence[0] is None:
-        return(strlen, None)
-    return(strlen, strfchar)
+    if len(sentence) == 0:
+        return(0, None)
+    return(len(sentence), sentence[0])
