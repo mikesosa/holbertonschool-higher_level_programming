b94522d1b81572613a3667e9385bd57a00932dab
Michael <michael.sosa@holbertonschool.com>
2019-09-19 12:15:13 -0500

    list comprehension

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    s1 = []
-    s2 = []
-    s3 = []
-    for i in matrix:
-        for j in i:
-            if len(s1) < 3:
-                s1 += [j**2]
-                continue
-            if len(s2) < 3:
-                s2 += [j**2]
-                continue
-            if len(s3) < 3:
-                s3 += [j**2]
-                continue
-    return [s1, s2, s3]
+    return [[y**2 for y in x] for x in matrix]
