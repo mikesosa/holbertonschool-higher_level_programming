1c031496bdc2b8898ebdff8cb463359b7b8cafd8
Michael <michael.sosa@holbertonschool.com>
2019-09-19 07:57:52 -0500

    Returning the sum of a unique list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    unique_list = []
+    for x in my_list:
+        if x not in unique_list:
+            unique_list.append(x)
+    return sum(unique_list)
