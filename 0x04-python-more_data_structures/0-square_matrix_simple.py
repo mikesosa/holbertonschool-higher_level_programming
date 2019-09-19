a483cd68d3233ccc37db21b3c29e9dd716fdcd4a
Michael <michael.sosa@holbertonschool.com>
2019-09-19 07:35:28 -0500

    Returning a new matrix, with pow values

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def square_matrix_simple(matrix=[]):
+    s1 = []
+    s2 = []
+    s3 = []
+    for i in matrix:
+        for j in i:
+            if len(s1) < 3:
+                s1 += [j**2]
+                continue
+            if len(s2) < 3:
+                s2 += [j**2]
+                continue
+            if len(s3) < 3:
+                s3 += [j**2]
+                continue
+    return(s1, s2, s3)
