fce51d44c72ddd54641d735c2de08c9c80144400
Michael <michael.sosa@holbertonschool.com>
2019-10-01 19:13:55 -0500

    Updating values when initialize

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -2,8 +2,8 @@
 class Square:
     """Test"""
     def __init__(self, size=0, position=(0, 0)):
-            self.__size = size
-            self.__position = position
+            self.size = size
+            self.position = position
 
     def area(self):
         return self.__size ** 2
