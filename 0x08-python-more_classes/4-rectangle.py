b97689ccee38e0cd0dfb497f1255c18ca6411e4b
Michael <michael.sosa@holbertonschool.com>
2019-10-07 12:16:27 -0500

    Adding __repr__ method

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/lib/python3
+class Rectangle:
+    """
+        Class that defines a Rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integeri")
+            return
+        if value < 0:
+            raise ValueError("width must be >= 0")
+            return
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integeri")
+            return
+        if value < 0:
+            raise ValueError("width must be >= 0")
+            return
+        self.__height = value
+
+    def area(self):
+        area = self.__width * self.__height
+        return area
+
+    def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        perimeter = self.__width + self.__height + self.__width + self.__height
+        return perimeter
+
+    def __str__(self):
+        str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                str = str + "#"
+            if i < (self.__height - 1):
+                str = str + "\n"
+        return str
+
+    def __repr__(self):
+        return ("Rectangle({}, {})".format(self.__width, self.__height))
