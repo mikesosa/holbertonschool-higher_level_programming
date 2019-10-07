614c4f3d8f44138cf3e8aa59819aecf570c7cc95
Michael <michael.sosa@holbertonschool.com>
2019-10-07 18:39:34 -0500

    Declaring private __init__ variables is not good :(

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         self.__class__.number_of_instances += 1
 
     @property
