dc1216e66b070154f08c48e84f67b1666d0dfa82
Michael <michael.sosa@holbertonschool.com>
2019-10-07 11:41:58 -0500

    If with or height is 0 periemter is 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -40,5 +40,7 @@ class Rectangle:
         return area
 
     def perimeter(self):
+        if self.__width is 0 or self.__height is 0:
+            return 0
         perimeter = self.__width + self.__height + self.__width + self.__height
         return perimeter
