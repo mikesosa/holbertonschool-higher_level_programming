8d80d020bb6a08a854ffb22f7eabda0e6b69d202
Michael <michael.sosa@holbertonschool.com>
2019-10-15 18:05:44 -0500

    Inherits from rectangles and creates a square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module has class square"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Initialize variables"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ returns area """
+        return (super().area())
