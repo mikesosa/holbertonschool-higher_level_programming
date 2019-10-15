a88ef46a659a8b0991614581ef8941f19fb178cf
Michael <michael.sosa@holbertonschool.com>
2019-10-15 16:12:19 -0500

    Is it instance of a class?

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module returns true or false"""
+
+
+def is_kind_of_class(obj, a_class):
+    """true if object is an instance of the class"""
+    return isinstance(obj, a_class)
