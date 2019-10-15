ef134b5c41bd76d883235960ddaa84e309a51f45
Michael <michael.sosa@holbertonschool.com>
2019-10-15 17:09:09 -0500

    Method that checks if integer

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhton3
+""" Module has class BaseGeometry """
+
+
+class BaseGeometry:
+    """I'll be 30 next year :v"""
+
+    def area(self):
+        """Raises error"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Cheks if value is integer"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greather than 0".format(name))
