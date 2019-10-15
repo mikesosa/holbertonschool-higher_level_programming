04fca809c58675cf0748a1d044d415c7dad52fe0
Michael <michael.sosa@holbertonschool.com>
2019-10-15 17:02:52 -0500

    Defining area method - not implemented yet

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pyhton3
+""" Module has class BaseGeometry """
+
+
+class BaseGeometry:
+    """I'll be 30 next year :v"""
+
+    def area(self):
+        """Raises error"""
+        raise Exception("area() is not implemented")
