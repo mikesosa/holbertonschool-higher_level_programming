e9763d52183f4370f802d24cf19b91039501d346
Michael <michael.sosa@holbertonschool.com>
2019-10-16 06:50:07 -0500

    Is none value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ error checks """
-        if type(value) is not int or not value:
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
