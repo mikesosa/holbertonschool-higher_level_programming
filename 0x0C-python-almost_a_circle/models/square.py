5f99042217e80f2165c05b86c2da1719e6f09e01
Michael <michael.sosa@holbertonschool.com>
2019-10-22 15:33:01 -0500

    Test

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Description of the module"""
+""" module that holds the square class. inherits from rect """
 
 from models.rectangle import Rectangle
 
@@ -8,13 +8,13 @@ class Square(Rectangle):
     """ all sides same size """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization"""
+        """ initializer """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Returns a description of the square"""
-        return "[Square] ({}) {}/{}\
- - {}".format(self.id, self.x, self.y, self.width)
+        """ overwrites the str """
+        return("[{}] ({}) {}/{} - {}".format(
+            type(self).__name__, self.id, self.x, self.y, self.width))
 
     @property
     def size(self):
@@ -23,46 +23,45 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        """ size setter, and validation"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-        self.__height = value\
+        """ setter """
+        self.typeChecker("width", value)
+        self.valueWHChecker("width", value)
+        self.width = value
 
+        self.typeChecker("height", value)
+        self.valueWHChecker("height", value)
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """Update square method"""
-        if args is not None and len(args):
-            for i, value in enumerate(args):
-                if i is 0:
+        """ the update method """
+        if len(args):
+            for index, value in enumerate(args):
+                if index is 0:
                     self.id = value
-                elif i is 1:
+                elif index is 1:
                     self.size = value
-                elif i is 2:
+                elif index is 2:
                     self.x = value
-                elif i is 3:
+                elif index is 3:
                     self.y = value
-                else:
-                    raise Exception("Too many arguments")
+                elif index >= 4:
+                    raise Exception("WTF TOO MANY")
         else:
-            for i in kwargs:
-                if i is "size":
-                    self.size = kwargs["size"]
-                elif i is "x":
-                    self.x = kwargs["x"]
-                elif i is "y":
-                    self.y = kwargs["y"]
-                elif i is "id":
+            for key in kwargs:
+                if key == "id":
                     self.id = kwargs["id"]
+                elif key == "size":
+                    self.size = kwargs["size"]
+                elif key == "x":
+                    self.x = kwargs["x"]
+                elif key == "y":
+                    self.y = kwargs["y"]
 
     def to_dictionary(self):
-        """Returns a description on a dict"""
-        my_dict = {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-        }
-        return my_dict
+        """ returns the dic representation of a Square """
+        temp = {}
+        temp['id'] = self.id
+        temp['size'] = self.size
+        temp['x'] = self.x
+        temp['y'] = self.y
+        return temp
