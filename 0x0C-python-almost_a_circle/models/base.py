9e50cc3fa16190dfe2c9222c46b63c0d825792f1
Michael <michael.sosa@holbertonschool.com>
2019-10-22 15:30:33 -0500

    Test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,71 +1,86 @@
 #!/usr/bin/python3
-"""Modula has base for all other classes"""
+""" module and base of all other classes """
 
 import json
 
 
-class Base:
-    """BaseClass for classes"""
+class Base():
+    """ BaseClass of all other classes """
 
-    _nb_objects = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
-        """Constructor/Initializer"""
+        """ instantiator """
         if id is not None:
             self.id = id
         else:
-            Base._nb_objects += 1
-            self.id = Base._nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """ Returns json of a dictionary """
-        if list_dictionaries is None or not len(list_dictionaries):
+        """ returns JSON of a dictionary """
+        if list_dictionaries is None or len(list_dictionaries) is 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        """writes json string to a file"""
-        ls = []
-        if list_objs is None or not len(list_objs):
+        """ writes the JSON string to a file """
+        temp = []
+        if list_objs is None or len(list_objs) is 0:
             pass
         else:
             for i in list_objs:
-                ls.append(cls.to_dictionary(i))
-        x = cls.to_json_string(ls)
+                temp.append(cls.to_dictionary(i))
+        x = cls.to_json_string(temp)
         with open("{}.json".format(cls.__name__),
-                  mode="w", encoding="utf-8") as f:
+                  mode='w', encoding='utf-8') as f:
             f.write(x)
 
     @staticmethod
     def from_json_string(json_string):
-        """returns the list of a json string"""
-        if json_string is None or not len(json_string):
-            return "[]"
-        else:
-            return json.loads(json_string)
+        """ returns the list of a json string """
+        if json_string is None or len(json_string) is 0:
+            return []
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """returns an instance with all attributes already set"""
+        """ returns an instance with all attributes of the dict """
         if cls.__name__ == "Rectangle":
-            dum = cls(width=6, height=9)
+            temp = cls(width=6, height=9)
         if cls.__name__ == "Square":
-            dum = cls(size=69)
-        dum.update(**dictionary)
-        return dum
+            temp = cls(size=69)
+        temp.update(**dictionary)
+
+        return temp
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances"""
-        ls = []
+        """ returns a list of instances """
+        temp = []
         try:
-            with open("{}.json".format(cls.__name__),
-                      mode="r", encoding='utf-8') as f:
-                temp = cls.from_json_string(f.read())
+            with open("{}.json".format(
+                    cls.__name__), "r", encoding='utf-8') as f:
+                temp2 = cls.from_json_string(f.read())
         except:
             return []
-        for i in temp:
-            ls.append(cls.create(**i))
-        return ls
+        for i in temp2:
+            temp.append(cls.create(**i))
+        return temp
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """ lol i didnt do dis """
+        pass
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """ I dont like unit tests"""
+        pass
+
+    @staticmethod
+    def draw(list_rectangles, list_squares):
+        """ i got too much other stuff to do. """
+        pass
