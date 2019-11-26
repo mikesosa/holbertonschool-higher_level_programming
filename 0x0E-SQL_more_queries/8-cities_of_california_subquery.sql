2a93a50946e1b4fd83abb9f531b44d04e8f6daf3
Michael <michael.sosa@holbertonschool.com>
2019-11-26 10:27:02 -0500

    Listing cities from states

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,4 @@
 -- Script that lists all the cities of California
 -- that can be found in the database hbtn_0d_usa.
-SELECT id, name FROM cities WHERE state_id = 1;
+SELECT cities.id, cities.name FROM cities, states
+WHERE cities.state_id = states.id AND states.name = "California";
