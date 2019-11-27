9fefb8193d2d2342014300834a417287d11e09c3
Michael <michael.sosa@holbertonschool.com>
2019-11-26 20:32:08 -0500

    LAST 3 TASKS :(

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- lists only the comedies
+SELECT tv_shows.title 'title'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id 
+WHERE tv_genres.name = "Comedy"
+ORDER BY title ASC
