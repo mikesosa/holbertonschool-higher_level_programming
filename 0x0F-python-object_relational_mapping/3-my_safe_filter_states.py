319e8c2c829e84573b4fb358d87704ea397adf2d
Michael <michael.sosa@holbertonschool.com>
2019-12-13 10:35:39 -0500

    Using literal values so no query injections ppssible

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+
+def getAllStates(username, password, db_name, query):
+    """ script that gets states queried by the user
+        and safe from query injections"""
+    # Creating connection
+    db = MySQLdb.connect(host='localhost', user=username, passwd=password,
+                         db=db_name, port=3306)
+    # Creating a cursor
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states
+                WHERE name=%s ORDER BY id ASC""", (query,))
+
+    states = cur.fetchall()
+    # Printing resutls
+    for state in states:
+        print(state)
+    # Close all cursors
+    cur.close()
+    # Close all databases
+    db.close()
+
+
+if __name__ == "__main__":
+    """ Protected from executing """
+    # Calling the function taking 3 arguments
+    getAllStates(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
