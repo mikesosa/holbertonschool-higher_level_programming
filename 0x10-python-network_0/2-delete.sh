fd756461209d7c8cce9a8b1a099c1c97eb9a97ab
Michael <michael.sosa@holbertonschool.com>
2020-01-06 13:51:19 -0500

    Documentation

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a GET request and posts the reponse
+# Bash script that takes in a URL, sends a DELETE request and posts the reponse
 curl -sL -X DELETE "$1"
