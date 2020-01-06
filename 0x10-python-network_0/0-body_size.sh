68fb3d1b49933f9a6110433076ce98a6d8d82e10
Michael <michael.sosa@holbertonschool.com>
2020-01-06 12:13:39 -0500

    Only 3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Script that returns the size in bites of an ip
-# by the info of the headers
+# Script that returns the size in bites of an ip by the info of the headers
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
