ddcf47367d9fcf8189140e9cc2123cef74bee923
Michael <michael.sosa@holbertonschool.com>
2020-01-13 17:55:14 -0500

    Update'

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,11 +6,6 @@ sends a request to the URL and displays the value of the X-Request-Id value
 import requests
 import sys
 
-
-def urlRequest(data):
-    """ Function that makes a request to a url. 67 days left """
-    req = requests.get(data)
-    print(req.headers['X-Request-Id'])
-
 if __name__ == "__main__":
-    urlRequest(sys.argv[1])
+    poop = requests.get(sys.argv[1])
+    print(poop.headers.get('X-Request-ID'))
