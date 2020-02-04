43ab9b85dfa0c568a2a01018abd0cc377c333b8a
mikesosa <michael.sosa@holbertonschool.com>
2020-02-04 04:32:57 -0500

    Task 3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
-// console.log(typeof(process.argv[0]))
-// console.log(process.argv[1])
-// console.log(process.argv[2])
-
 if (process.argv[3]) {
   console.log('Arguments found');
 } else if (process.argv[2]) {
