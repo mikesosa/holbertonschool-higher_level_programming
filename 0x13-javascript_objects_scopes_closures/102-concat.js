04fcf6c4d8bc1696d5c1da6528deb7d85191d5c3
mikesosa <michael.sosa@holbertonschool.com>
2020-02-10 17:32:01 -0500

    Update

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-let c = fs.readFileSync(process.argv[2]);
-c += fs.readFileSync(process.argv[3]);
-fs.writeFile(process.argv[4], c);
+const data1 = fs.readFileSync(process.argv[2], 'utf8');
+const data2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], data1 + data2);
