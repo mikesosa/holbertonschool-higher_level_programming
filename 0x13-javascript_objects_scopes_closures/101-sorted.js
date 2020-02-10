abb05828dcff1709f0ab6dbcf6ec73668e26a46a
mikesosa <michael.sosa@holbertonschool.com>
2020-02-10 17:18:26 -0500

    Chmod

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 let temp = require('./101-data');
 temp = temp.dict;
-let MYDICT = {};
-for (let key in temp) {
+const MYDICT = {};
+for (const key in temp) {
   // MYDICT[temp[i]].push(temp[i]);
   // TypeError: Cannot read property 'push' of undefined FUUUU
   if (MYDICT[temp[key]] === undefined) {
