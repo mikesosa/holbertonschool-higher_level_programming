9d2a6524dd5e6ddfed99fd11aaa84270d84a1a0f
mikesosa <michael.sosa@holbertonschool.com>
2020-02-10 17:16:50 -0500

    never VAR

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 
 exports.esrever = function (list) {
   const newArray = [];
-  for (var i = list.length - 1; i >= 0; i--) {
+  for (let i = list.length - 1; i >= 0; i--) {
     newArray.push(list[i]);
   }
   return newArray;
